d314335421a56b713bca39dcf2974f3aff2a1688
Łukasz Magiera <magik6k@gmail.com>
2023-05-08 22:19:29 +0200

    ribsweb: More digits in formatted numbers

--- integrations/web/ribswebapp/src/helpers/fmt.js
+++ integrations/web/ribswebapp/src/helpers/fmt.js
@@ -4,7 +4,7 @@ export function formatBytesBinary(bytes) {
     while (n >= 1024 && ++l) {
         n = n / 1024;
     }
-    return (n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + units[l]);
+    return (n.toFixed(l > 0 ? 2 : 0) + ' ' + units[l]);
 }
 
 export function formatBitsBinary(bytes) {
@@ -13,7 +13,7 @@ export function formatBitsBinary(bytes) {
     while (n >= 1024 && ++l) {
         n = n / 1024;
     }
-    return (n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + units[l]);
+    return (n.toFixed(l > 0 ? 2 : 0) + ' ' + units[l]);
 }
 
 export function formatNum(bytes) {
@@ -22,7 +22,7 @@ export function formatNum(bytes) {
     while (n >= 1000 && ++l) {
         n = n / 1000;
     }
-    return (n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + units[l]).trim();
+    return (n.toFixed(l > 0 ? 2 : 0) + ' ' + units[l]).trim();
 }
 
 export function formatNum6(bytes) {
